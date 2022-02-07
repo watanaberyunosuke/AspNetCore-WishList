456fcba5aadf4bb50d4154dd2049fb2299bc8963
watanaberyunosuke <harrybwatson@protonmail.com>
2022-02-07 14:42:35 +1100

    Task 3

---
+++ WishList/Views/Home/Error.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views.Home
+{
+    public class Error : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ WishList/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views.Home
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
