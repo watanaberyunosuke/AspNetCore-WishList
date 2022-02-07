79a87aa212af0a302670862b0e3c0a9bff00d994
watanaberyunosuke <harrybwatson@protonmail.com>
2022-02-07 21:37:32 +1100

    Task 5 debug

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
